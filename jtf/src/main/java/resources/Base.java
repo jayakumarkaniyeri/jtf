efe831c5d58843fbfc2d33c1654ff6f6989c8e27
jayakumarkaniyeri <jayakumarkaniyeri@localhost>
2020-11-08 23:37:09 +0000

    jenkins integration done

--- jtf/src/main/java/resources/Base.java
+++ jtf/src/main/java/resources/Base.java
@@ -11,6 +11,7 @@ import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.safari.SafariDriver;
 
 public class Base {
@@ -25,7 +26,8 @@ public class Base {
 		FileInputStream fis = new FileInputStream(projectPath + "//src//main//java//resources//data.properties");
 		
 		prop.load(fis);
-		String browserName = prop.getProperty("browser");
+		//String browserName = prop.getProperty("browser");
+		String browserName = System.getProperty("browser");
 		
 		if(browserName.equals("chrome")) {
 			System.setProperty("webdriver.chrome.driver", projectPath + "//src//main//java//resources//drivers//chromedriver");
@@ -34,12 +36,15 @@ public class Base {
 		}
 		
 		else if(browserName.equals("firefox")) {
-			//firefox code
+			System.setProperty("webdriver.gecko.driver", projectPath + "//src//main//java//resources//drivers//geckodriver");
+			driver = new FirefoxDriver();
+		
 		}
 		
 		else if(browserName.equals("safari")) {
 			System.setProperty("webdriver.safari.driver", projectPath + "//src//main//java//resources//drivers//safaridriver");
 			driver = new SafariDriver();
+			//driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
 		}
 		
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
